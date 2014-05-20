59666eadcec0e071292406c86caadfba20635749
Dario Seyb <dario.seyb@gmail.com>
2014-05-20 19:44:49 +0200

    -bird sprites

---
+++ ydwptt/Assets/Scripts/Bird/BirdSpriteChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdSpriteChanger : MonoBehaviour
+{
+    public Sprite UpSprite;
+    public Sprite DownSprite;
+
+    public BirdController birdController;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (birdController.vel.y < 0)
+	    {
+	        (renderer as SpriteRenderer).sprite = DownSprite;
+	    }
+	    else
+	    {
+            (renderer as SpriteRenderer).sprite = UpSprite;
+	    }
+	}
+}
